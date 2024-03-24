15e33d64e7fc560e395dd0660e197fd3db070b5e
OdedItkinOW <oded.itkin@overwolf.com>
2024-03-24 21:05:37 +0200

    Fixed typo

--- src/components/position-table/index.tsx
+++ src/components/position-table/index.tsx
@@ -1,11 +1,9 @@
 import Table from './table';
 import TableCell from './table-cell';
 import TableHeader from './table-header';
-import TableSubCell from './table-sub-cell';
 
 export default {
   Table,
   TableCell,
-  TableSubCell,
   TableHeader,
 };
